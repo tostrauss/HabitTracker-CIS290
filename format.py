3cc25f987852bd1144023b36463c88bdde90468b
tostrauss <tobias.p.strauss@gmail.com>
2025-02-24 07:26:42 +0100

    trening

---
+++ format.py
@@ -0,0 +1,27 @@
+# trening 24/02 TS
+
+import os
+import sys
+import re
+
+name = input("Enter your name: ").strip()
+# if "," in name:
+    # last, first = name.split(", ?")
+    # name = f"{first.strip()} {last.strip()}"
+if re.search(r"\d$", name):
+    print("Your name must not contain a number")
+    sys.exit(1)
+if not name:
+    print("You must enter a name")
+    sys.exit(1)
+print(f"Hello, {name}")
+
+match  = re.search(r"^(.+), (.+)$", name)
+if match:
+    last = match.group(1)
+    first = match.group(2)
+    name = f"{first} {last}"
+    print(f"Hello, {first} {last}")
+else:
+    print(f"Hello, {name}")
+
